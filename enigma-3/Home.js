3f254d9b1b63ef84f15014023b75460de354211b
Sarthak Pranesh <sarthakpranesh08@gmail.com>
2019-02-10 10:50:34 +0530

    updates
    Added onload animation on the wizard

---
+++ enigma-3/Home.js
@@ -0,0 +1,16 @@
+function man() {
+    if (window.innerWidth >= 760) {
+        var ele = document.getElementById("enigma-man");
+        var pos = 0;
+        var id = setInterval(frame, 1);
+
+        function frame() {
+            if (pos == 510) {
+                clearInterval(id);
+            } else {
+                pos++;
+                ele.style.width = pos + "px";
+            }
+        }
+    }
+}
