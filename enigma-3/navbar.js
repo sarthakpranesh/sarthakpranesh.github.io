cdbc239fd5409195eb5c7c5639269f3b6ae82d7c
Sarthak Pranesh <sarthakpranesh08@gmail.com>
2019-02-12 00:09:28 +0530

    updates

--- enigma-3/navbar.js
+++ enigma-3/navbar.js
@@ -3,9 +3,11 @@ window.onscroll = function() {addEnigma()};
 function addEnigma() {
     if (window.innerWidth >= 760) {
         if (document.body.scrollTop > 400 * window.devicePixelRatio || document.documentElement.scrollTop > 400 * window.devicePixelRatio) {
-            document.getElementById("enigma-on-nav").style.display = "block";
-        } else {
-            document.getElementById("enigma-on-nav").style.display = "none";
+            document.getElementById("nav-top").classList.add()
         }
+    }
+    else {
+        document.getElementById("nav-top").classList.add(navbar-dark);
+        document.getElementById("nav-top").classList.add(bg-dark);
     }
 }
